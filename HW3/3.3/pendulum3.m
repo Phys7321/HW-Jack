1ef41372ea4407262ee9d61be7e5e89830633388
Jingyan95 <43122048+Jingyan95@users.noreply.github.com>
2018-10-21 18:55:54 -0400

    Add files via upload

--- HW3/3.3/pendulum3.m
+++ HW3/3.3/pendulum3.m
@@ -1,4 +1,4 @@
-function [t,w] = pendulum3(R,theta0,thetad0,gamma) 
+function [t,w,te] = pendulum3(R,theta0,thetad0,gamma) 
 % Finds the period of a nonlinear pendulum given the length of the pendulum
 % arm and initial conditions. All angles in radians.
 
@@ -18,16 +18,3 @@ end
 function rdot = proj(t,r,g,R,gamma)
     rdot = [r(2); -g/R*sin(r(1))-gamma*r(2)];
 end
-
-%function [value,isterminal,direction] = events(t,w)
-
-% Locate the time when height passes through zero in
-
-% a decreasing direction and stop integration.  
-%if abs(w(1))-0.0001<0
-    %else
-    %value=1;
-%end
-%isterminal = 1;% stop  
-%direction = 0; % any direction
-%end
